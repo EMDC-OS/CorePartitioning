3bb0d3ab9fddf5920c01c234d9296658897a6f4d
dylan0924 <changyu.lee.02@gmail.com>
2024-12-31 07:50:31 +0900

    debug header

--- debug.h
+++ debug.h
@@ -4,26 +4,31 @@
 
 // debug.h for Core partitioning debug mode
 
+// debug system call's input/output/etc
 #ifdef __DEBUG_SYSCALL__
 #define DEBUG_SYSCALL(X, ...) log_fd = fopen("/home/hadoop/CP_log/CP_debug_function.txt","a"); fprintf(log_fd, X, ##__VA_ARGS__); fclose(log_fd);
 #else
 #define DEBUG_SYSCALL(X, ...)
 #endif
 
+// debug syscall thread
 #ifdef __DEBUG_SYSTHREAD__
 #define DEBUG_SYSTHREAD(X, ...) log_fd = fopen("/home/hadoop/CP_log/CP_debug_systhread.txt","a"); fprintf(log_fd, X, ##__VA_ARGS__); fclose(log_fd);
 #else
 #define DEBUG_SYSTHREAD(X, ...)
 #endif
 
+// debug fdtable management
 #ifdef __DEBUG_FDTABLE__
 #define DEBUG_FDTABLE(X, ...) log_fd = fopen("/home/hadoop/CP_log/CP_debug_fdtable.txt","a"); fprintf(log_fd, X, ##__VA_ARGS__); fclose(log_fd);
 #else
 #define DEBUG_FDTABLE(X, ...)
 #endif
 
+// debug thread pool for fd table
 #ifdef __DEBUG_POOL__
 #define DEBUG_POOL(X, ...) log_fd = fopen("/home/hadoop/CP_log/CP_debug_pool.txt","a"); fprintf(log_fd, X, ##__VA_ARGS__); fclose(log_fd);
 #else
 #define DEBUG_POOL(X, ...)
 #endif
+
